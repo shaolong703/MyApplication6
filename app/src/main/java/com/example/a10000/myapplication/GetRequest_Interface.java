5185ab445024d64377e83634074ddd1079cd41ae
shaolong <shaolong@focalcrest.com>
2018-08-15 18:41:19 +0800

    第四次冲突提交测试

--- app/src/main/java/com/example/a10000/myapplication/GetRequest_Interface.java
+++ app/src/main/java/com/example/a10000/myapplication/GetRequest_Interface.java
@@ -11,4 +11,5 @@ public interface GetRequest_Interface {
     // Retrofit把网络请求的URL分成了两部分：一部分放在Retrofit对象里，另一部分放在网络请求接口里
     // 如果接口里的url是一个完整的网址，那么放在Retrofit对象里的URL可以忽略
     // getCall()是接受网络请求数据的方法
+    // getCall()是接受网络请求数据的方法
 }
